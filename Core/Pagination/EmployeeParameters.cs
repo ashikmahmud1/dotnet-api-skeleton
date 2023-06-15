8499286ac7602367359c7be270918a33a3249c62
ashikmahmud137@gmail.com <ashikmahmud137@gmail.com>
2023-06-15 21:58:22 +0600

    Added filtering and sorting functionality

--- Core/Pagination/EmployeeParameters.cs
+++ Core/Pagination/EmployeeParameters.cs
@@ -2,6 +2,14 @@ namespace Core.Pagination
 {
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+
+        public uint MaxAge { get; set; } = int.MaxValue;
+        public bool ValidAgeRange
+        {
+            get => MaxAge > MinAge;
+        }
         
+        public string SearchTerm { get; set; }
     }
 }
--- Infrastructure/Repositories/EmployeeRepository.cs
+++ Infrastructure/Repositories/EmployeeRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Pagination;
+using Infrastructure.Repositories.Extensions;
 using Microsoft.EntityFrameworkCore;
 namespace Infrastructure.Repositories
 {
@@ -17,10 +18,12 @@ namespace Infrastructure.Repositories
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                .Search(employeeParameters.SearchTerm)
                 .OrderBy(e => e.Name)
-                .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize) .Take(employeeParameters.PageSize)
+                .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize).Take(employeeParameters.PageSize)
                 .ToListAsync();
-            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges ).CountAsync();
+            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
             return new PagedList<Employee>(employees, count,
                 employeeParameters.PageNumber, employeeParameters.PageSize);
         }
@@ -33,7 +36,7 @@ namespace Infrastructure.Repositories
             employee.CompanyId = companyId;
             Create(employee);
         }
-        
+
         public void DeleteEmployee(Employee employee) => Delete(employee);
     }
 }
